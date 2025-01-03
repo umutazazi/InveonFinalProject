77b4e415381072826fc0bdac5f0c55c788c2bdfc
Umut Azazi <72599457+umutazazi@users.noreply.github.com>
2025-01-03 17:21:09 +0300

    migration eklendi

--- Inveon.Repository/obj/Debug/net9.0/Inveon.Repository.AssemblyInfo.cs
+++ Inveon.Repository/obj/Debug/net9.0/Inveon.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Inveon.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075ca40c09b2742eeb9a6e6a812e080496e76c29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd42178b0915c62772a218bdd7c8a851834273c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Inveon.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Inveon.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
